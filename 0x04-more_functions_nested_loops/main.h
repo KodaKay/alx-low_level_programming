e02d9f626efbac2ba1feb40d9880c460c019b58d
KodaKay <stephen.ogbonaiye@gmail.com>
2022-09-16 07:20:09 -0700

    addd main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/*
+ * file - main.h
+ * Author: Stephen Ogbonaiye 
+ * Desc: Header contains prototype for all functions
+ * 	used in 0x04-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+#endif
